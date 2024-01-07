9376ba92988b70f49bfa218d2840343d65b25f4f
MAYM7 <mmkoko8@gmail.com>
2024-01-06 17:50:55 -0800

    adding fork and execute function

---
+++ execomand.c
@@ -0,0 +1,46 @@
+#include "maym.h"
+/**
+ * execute_com - excute command.
+ * @command: the command.
+ */
+
+void execute_com(char *args[], int background)
+{
+	pid_t pid = fork();
+
+	if (pid == -1)
+	{
+		perror("Fork failed");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		if (background)
+		{
+			signal(SIGINT, SIG_IGN);
+		}
+
+		execvp(args[0], args);
+
+		perror("Execvp failed");
+			exit(EXIT_FAILURE);
+	}
+	else
+	{
+		if (!background)
+		{
+			int status;
+
+			waitpid(pid, &status, 0);
+
+			if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+			{
+				fprintf(stderr, "Error: Command '%s' not found.\n", args[0]);
+			}
+		}
+		else
+		{
+			printf("[%d] %s\n", pid, args[0]);
+		}
+	}
+}

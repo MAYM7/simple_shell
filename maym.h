b5ba1b201e56bd8ae2205d22b890c4fe0e8d8168
MAYM7 <mmkoko8@gmail.com>
2024-01-06 17:54:16 -0800

    declaring all functions and adding liberies

--- maym.h
+++ maym.h
@@ -5,5 +5,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <fcntl.h>
+
+#define MAX_INPUT_SIZE 100
+#define MAX_ARGS 10
+
+void handle_signal(int signo);
+void execute_com(char *args[], int background);
 
 #endif
